5c6607a4b90fbef16d8a159674a2b2fdb12433ad
MatthewPon <mattpon13@gmail.com>
2020-02-10 00:49:03 -0600

    Added home page & log in page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as $ from "jquery";
+import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import hash from "./hash";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The Spotify Toolkit
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//taken from https://github.com/JoeKarlsson/react-spotify-player
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null
+  }
+}
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className = "App-name"> The Spotify Toolkit </h1>
+          <Button variant="contained"  href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`} 
+              className="App-button">
+  Log In
+</Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
